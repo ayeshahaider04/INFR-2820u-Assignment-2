4acdb034f339fb2540111a47050d93f710ce672a
ayeshahaider04 <ayesha.haider@ontariotechu.net>
2024-07-24 19:17:35 -0400

    Update algorithms and data structures - assignment 2.py

--- algorithms and data structures - assignment 2.py
+++ algorithms and data structures - assignment 2.py
@@ -1,13 +1,16 @@
 # Assignment 2 
 # Ayesha Haider 100869659
+
+import time
+import winsound
 def MergeSort(arr, data=0):
     if len(arr) > 1:
         middle = len(arr) // 2  
         first_half = arr[:middle]  
-        second_half = arr[middle:]
-        i = j = k = 0 
+        second_half = arr[middle:] 
         MergeSort(first_half, data + 1)  
         MergeSort(second_half, data + 1)  
+        i = j = k = 0
 
         while i < len(first_half) and j < len(second_half):
             if first_half[i] < second_half[j]:
@@ -22,12 +25,13 @@ def MergeSort(arr, data=0):
             arr[k] = first_half[i]
             i += 1
             k += 1
+            winsound.Beep(1000, 1000)
 
         while j < len(second_half):
             arr[k] = second_half[j]
             j += 1
             k += 1
-
+            
         print(" " * data * 2 + f"first half during sorting: {first_half}")
         print(" " * data * 2 + f"second half during sorting: {second_half}")
         print(" " * data * 2 + f"Merged array sorted in increasing order: {arr}")
@@ -45,4 +49,3 @@ if __name__ == "__main__":
     PrintArray(arr)
     
     MergeSort(arr)
-
